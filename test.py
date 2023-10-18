b1040212d8f9afdb424fd61c6a3c0f71c78ef4f6
Chandraparsad <chandraparsad02@gmail.com>
2023-10-18 18:25:58 +0530

    generator founction

---
+++ test.py
@@ -0,0 +1,12 @@
+def test():
+    pass 
+#The pass keyword can
+# be used in Python 
+# for loop, when a user
+#  doesn't know what 
+# to code inside the loop in Python
+
+a,b=5,7
+print(a)
+
+print(b)
